abac110a99a20f4fdc04c72c8b2b73104e913944
krasimir-zhelezov <krasizh07@gmail.com>
2025-06-11 15:53:15 +0300

    feat(backend): search endpoint

---
+++ backend/app/api/v1/endpoints/webscraper.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from app.services.webscraper import Webscraper
+from typing import List
+
+router = APIRouter(prefix='/webscraper', tags=['webscraper'])
+webscraper = Webscraper()
+
+@router.get('/search/{query}')
+async def search(query: str) -> List[str]:
+    return webscraper.search(query)
---
+++ backend/app/services/webscraper.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from typing import List
+
+class Webscraper:
+    
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        
+    def search(self, query: str) ->  List[str]:
+        links = []
+        self.driver.get(f'https://kaksepishe.com/?s={query}&type=title')
+        
+        articles = self.driver.find_elements(By.TAG_NAME, 'article')
+        
+        for article in articles:
+            link = article.find_element(By.TAG_NAME, 'a').get_attribute('href')
+            links.append(link)
+            
+        return links
