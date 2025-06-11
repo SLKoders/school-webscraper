8ed5f6eb9bbcef882d5ce7aaf1ee6d9a484d6beb
krasimir-zhelezov <krasizh07@gmail.com>
2025-06-11 16:01:04 +0300

    fix(backend): searching returns single articles correctly

--- backend/app/services/webscraper.py
+++ backend/app/services/webscraper.py
@@ -11,10 +11,13 @@ class Webscraper:
         links = []
         self.driver.get(f'https://kaksepishe.com/?s={query}&type=title')
         
-        articles = self.driver.find_elements(By.TAG_NAME, 'article')
-        
-        for article in articles:
-            link = article.find_element(By.TAG_NAME, 'a').get_attribute('href')
-            links.append(link)
+        if '?s=' in self.driver.current_url:
+            articles = self.driver.find_elements(By.TAG_NAME, 'article')
+            
+            for article in articles:
+                link = article.find_element(By.TAG_NAME, 'a').get_attribute('href')
+                links.append(link)
+        else:
+            links.append(self.driver.current_url)
             
         return links
