97b6907228c2fce58ba188b75aff5802381499ca
krasimir-zhelezov <krasizh07@gmail.com>
2025-06-14 15:42:54 +0300

    fix(backend): improved webscraper code

--- backend/app/services/webscraper/bulgarian.py
+++ backend/app/services/webscraper/bulgarian.py
@@ -14,13 +14,11 @@ class BulgarianWebscraper(BaseWebscraper):
         if '?s=' in self.driver.current_url:
             articles = self.driver.find_elements(By.TAG_NAME, 'article')
             
-            for article in articles:
-                link = article.find_element(By.TAG_NAME, 'a').get_attribute('href')
-                links.append(link)
+            links = [article.find_element(By.TAG_NAME, 'a').get_attribute('href') for article in articles]
         else:
             links.append(self.driver.current_url)
             
-        return links
+        return [link for link in links if link is not None]
     
     def extract_page(self, link: str) -> str:
         self.driver.get(link)
