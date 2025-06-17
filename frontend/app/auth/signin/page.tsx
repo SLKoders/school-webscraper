c43267a0883cd705e020d72993a3252a9d17438b
krasimir-zhelezov <krasizh07@gmail.com>
2025-06-17 23:11:03 +0300

    feat(frontend): connected sign in form with backend

--- frontend/app/auth/signin/page.tsx
+++ frontend/app/auth/signin/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import { Input } from "@/components/ui/input"
+import Link from 'next/link';
+import { postRequest } from "@/lib/api";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -15,8 +16,11 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import * as z from "zod"
 import { Button } from "@/components/ui/button"
+import { useRouter } from "next/navigation";
 
 export default function SignIn() {
+  const router = useRouter();
+
   const formSchema = z.object({
     email: z.string().min(2, {
         message: "Email must be at least 2 characters.",
@@ -34,10 +38,23 @@ export default function SignIn() {
     },
   })
 
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    const response = await postRequest("auth/signin", {
+      email: values.email,
+      password: values.password,
+    })
+
+    if (response.status === 200) {
+      router.push('/');
+    }
+
+    console.log(response.data);
+  }
+
   return (
     <div className="flex items-center justify-center min-h-screen text-center">
       <Form {...form}>
-        <form className="space-y-8">
+        <form onSubmit={form.handleSubmit(onSubmit)}  className="space-y-8">
           <FormField
             control={form.control}
             name="email"
---
+++ frontend/lib/api.ts
@@ -0,0 +1,48 @@
+import axios, { AxiosResponse, AxiosError, AxiosRequestConfig } from 'axios';
+
+const baseUrl = "http://127.0.0.1:8000/api/";
+
+interface ApiResponse<T> {
+  data: T;
+  status?: number;
+  headers?: Record<string, string>;
+}
+
+// interface ApiError {
+//   data?: unknown;
+//   status?: number;
+//   headers?: Record<string, string>;
+//   message?: string;
+// }
+
+export async function postRequest<T>(
+  url: string,
+  payload: object,
+  config?: AxiosRequestConfig
+): Promise<ApiResponse<T>> {
+  try {
+    const response: AxiosResponse<T> = await axios.post(baseUrl + url, payload, config);
+    return {
+      data: response.data,
+      status: response.status,
+      headers: response.headers as Record<string, string>
+    };
+  } catch (error) {
+    const axiosError = error as AxiosError;
+    if (axiosError.response) {
+      throw {
+        data: axiosError.response.data,
+        status: axiosError.response.status,
+        headers: axiosError.response.headers as Record<string, string>
+      };
+    } else if (axiosError.request) {
+      throw {
+        message: 'No response received from the server'
+      };
+    } else {
+      throw {
+        message: axiosError.message
+      };
+    }
+  }
+}
