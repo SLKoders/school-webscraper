16999dab1f29c131891a5f0c1d031d0ba0cdc7b8
krasimir-zhelezov <krasizh07@gmail.com>
2025-06-17 23:06:20 +0300

    feat(backend): cors config

--- backend/schoolwebscraper/settings.py
+++ backend/schoolwebscraper/settings.py
@@ -45,9 +45,11 @@ INSTALLED_APPS = [
     'drf_yasg',
     'rest_framework',
     'accounts',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,6 +57,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://192.168.168.19:3000"
 ]
 
 ROOT_URLCONF = 'schoolwebscraper.urls'
