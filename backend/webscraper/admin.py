9248083299d1abd48b44368d9d9600ffaba03633
krasimir-zhelezov <krasizh07@gmail.com>
2025-06-18 13:59:57 +0300

    feat(backend): admin panel question/response view

--- backend/webscraper/admin.py
+++ backend/webscraper/admin.py
@@ -5,16 +5,39 @@ from .models import Question, Response, ResponseItem
 
 # admin.site.register(Question)
 
+# @admin.register(Question)
+# class QuestionModelAdmin(admin.ModelAdmin):
+#     list_display = ('category', 'question', 'user', 'created_at')
+#     search_fields = ('question', 'category')
+#     list_filter = ('category',)
+    
+# @admin.register(Response)
+# class ResponseModelAdmin(admin.ModelAdmin):
+#     list_display = ('question', 'created_at')
+    
+# @admin.register(ResponseItem)
+# class ResponseItemModelAdmin(admin.ModelAdmin):
+#     list_display = ('response', 'url', 'text')
+
+class ResponseItemInline(admin.StackedInline):  # or admin.StackedInline
+    model = ResponseItem
+    extra = 1  # Number of empty forms to display
+
+class ResponseInline(admin.StackedInline):
+    model = Response
+    extra = 1
+    show_change_link = True  # Allows clicking through to edit the Response
+    inlines = [ResponseItemInline]  # Nested inlines (Django 3.0+)
+
 @admin.register(Question)
-class QuestionModelAdmin(admin.ModelAdmin):
-    list_display = ('category', 'question', 'user', 'created_at')
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ResponseInline]
+    list_display = ('question', 'category', 'user', 'created_at')
     search_fields = ('question', 'category')
-    list_filter = ('category',)
-    
+    list_filter = ('category', 'created_at')
+
 @admin.register(Response)
-class ResponseModelAdmin(admin.ModelAdmin):
+class ResponseAdmin(admin.ModelAdmin):
+    inlines = [ResponseItemInline]
     list_display = ('question', 'created_at')
-    
-@admin.register(ResponseItem)
-class ResponseItemModelAdmin(admin.ModelAdmin):
-    list_display = ('response', 'url', 'text')
+    search_fields = ('question__question',)
