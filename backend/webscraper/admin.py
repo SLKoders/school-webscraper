6c85fb5955030ca2789d79c5ae23e412fc6f44ba
krasimir-zhelezov <krasizh07@gmail.com>
2025-06-18 14:41:52 +0300

    feat(backend): questions ordered by date

--- backend/webscraper/admin.py
+++ backend/webscraper/admin.py
@@ -9,6 +9,7 @@ class ArticleInline(admin.StackedInline):
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [ArticleInline]
+    ordering = ['-created_at']
     list_display = ('question', 'category', 'user', 'created_at')
     search_fields = ('question', 'category')
     list_filter = ('category', 'created_at')
